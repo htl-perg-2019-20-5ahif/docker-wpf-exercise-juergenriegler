f96a329772c6c344e88008624bb423cebe491871
unknown <juergen.riegler@gmx.net>
2020-01-22 22:04:51 +0100

    WPF UI: finished

---
+++ CarBookingApp/CarBookingApp/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using CarBookingAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CarBookingApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string ApiBaseAddress = "https://localhost:5001/api/";
+        private static readonly HttpClient Client = new HttpClient() { BaseAddress = new Uri(ApiBaseAddress) };
+
+        public ObservableCollection<Car> Cars { get; set; } = new ObservableCollection<Car>();
+        public ObservableCollection<Car> CarsAvailable { get; set; } = new ObservableCollection<Car>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            _ = LoadCarsAsync();
+        }
+
+        private async Task LoadCarsAsync()
+        {
+            var ResultCars = await GetCars();
+            ResultCars.ForEach(c => Cars.Add(c));
+
+        }
+
+        private async Task<List<Car>> GetCars()
+        {
+            var response = await Client.GetAsync("cars");
+            response.EnsureSuccessStatusCode();
+            var body = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<List<Car>>(body);
+        }
+
+        private void RefreshButtonClicked (object sender, RoutedEventArgs e) 
+        {
+            _ = RefreshAvailableCarsOnDateAsync();
+        }
+
+        private async Task RefreshAvailableCarsOnDateAsync()
+        {
+            if (!AvailabilityDatePicker.SelectedDate.HasValue) return;
+            var response = await Client.GetAsync("cars/available?date=" + AvailabilityDatePicker.SelectedDate.ToString());
+            response.EnsureSuccessStatusCode();
+            var body = await response.Content.ReadAsStringAsync();
+            var ResultCars = JsonSerializer.Deserialize<List<Car>>(body);
+            CarsAvailable.Clear();
+            ResultCars.ForEach(c => CarsAvailable.Add(c));
+        }
+
+
+        private void BookButtonClicked (object sender, RoutedEventArgs e) 
+        {
+            _ = BookCarAsync();
+        }
+
+        private async Task BookCarAsync()
+        {
+            if (BookingCarIdTextBox.Text == null) return;
+            if (!BookingDateDatePicker.SelectedDate.HasValue) return;
+
+            Booking booking = new Booking {
+                Date = BookingDateDatePicker.SelectedDate.Value,
+                CarId = Int32.Parse(BookingCarIdTextBox.Text)
+            };
+
+            var body = new StringContent(JsonSerializer.Serialize(booking), Encoding.UTF8, "application/json");
+            var responseMessage = await Client.PostAsync("bookings", body);
+            responseMessage.EnsureSuccessStatusCode();
+        }
+    }
+}
---
+++ CarBookingApp/CarBookingApp/Model.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CarBookingAPI.Model
+{
+    public class Car
+    {
+        [JsonPropertyName("id")]
+        public int ID { get; set; }
+
+        [JsonPropertyName("carmake")]
+        public string Carmake { get; set; }
+
+
+        [JsonPropertyName("carModel")]
+        public string CarModel { get; set; }
+
+
+        [JsonPropertyName("carIdentificationNumber")]
+        public string CarIdentificationNumber { get; set; }
+
+    }
+
+    public class Booking
+    {
+
+        [JsonPropertyName("id")]
+        public int ID { get; set; }
+
+        [JsonPropertyName("date")]
+        public DateTime Date { get; set; }
+
+        [JsonPropertyName("carId")]
+        public int CarId { get; set; }
+
+    }
+
+}
